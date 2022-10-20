644302038382c90d6a1ffb76943abd178b0a75b3
Srahejjai <sarahejjai12345@gmail.com>
2022-10-20 15:36:53 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_line - prints a straight line using '-'
+ * print_line - print a straight line using '-'
  *  @n: the number of '-' to be printed
  */
 void print_line(int n)
