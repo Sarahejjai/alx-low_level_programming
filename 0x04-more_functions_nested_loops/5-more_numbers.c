fbc488fbc47033e16223e28feccb21e205074260
Srahejjai <sarahejjai12345@gmail.com>
2022-10-20 13:28:56 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0-14 tine times.
+ */
+
+void more_numbers(void)
+{
+         int n, count;
+
+	 for (count = 0; count <= 9; count++)
+	 {
+	 if (n = 0; n <= 14; n++)
+	 {
+	 if (n > 9)
+	 {
+	 _putchar((n % 10) + '0');
+	 }
+	 _putchar((n % 10) + '0');
+	 }
+	 _putchar('\n');
+	 }
+}
+		 
+	
+
