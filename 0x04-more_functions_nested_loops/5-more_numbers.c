d949d497f3abeb2e5c48733a28efc3422c7de1ec
Srahejjai <sarahejjai12345@gmail.com>
2022-10-20 17:21:59 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,16 @@
 
 void more_numbers(void)
 {
-         int n, count;
+	int n, count;
 
-	 for (count = 1; count <= 9; count++)
-	 {
-	 for (n = 0; n <= 14; n++)
-	 {
-	 if (n > 9)
-	 _putchar('1');
-	_putchar((n % 10) + '0');
-	 }
-	_putchar('\n');
-	 }
+	for (count = 1; count <= 9; count++)
+	{
+		for (n = 0; n <= 14; n++)
+{
+			if (n > 9)
+				_putchar('1');
+			_putchar((n % 10) + '0');
+}
+		_putchar('\n');
+	}
 }
