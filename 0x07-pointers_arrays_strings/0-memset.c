89cd0c5c62c46c882418eb863b861a7849d24c1c
Srahejjai <sarahejjai12345@gmail.com>
2022-10-31 06:02:23 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * _strchr - Locates a character in a string.
  * @s: The string to be searched.
  * @c: The character to be located.
- * Return: If c is found - a pointer to the first occurence. 
+ * Return: If c is found - a pointer to the first occurence.
  * If c is not found - NULL.
  */
 
